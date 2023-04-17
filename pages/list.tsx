8e946030f3ba82cea56345d7e5bd2095a00de1dd
Taryn Riera <tarynriera@gmail.com>
2023-04-17 15:03:01 -0400

    added data grid to list page

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,14 +1,51 @@
 import { Box } from "@mui/material";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 
 export default function List() {
+  const rows: GridRowsProp = [
+    {
+      id: 1,
+      genus: "Galanthus",
+      species: "nivalis",
+      commonName: "snowdrop",
+      lat: 39.95,
+      long: -75.21,
+    },
+    {
+      id: 2,
+      genus: "Crocus",
+      species: "sativus",
+      commonName: "crocus",
+      lat: 39.95,
+      long: -75.21,
+    },
+    {
+      id: 3,
+      genus: "Helleborus",
+      species: null,
+      commonName: "lenten rose",
+      lat: 39.95,
+      long: -75.21,
+    },
+  ];
+
+  const columns: GridColDef[] = [
+    { field: "genus", headerName: "Genus" },
+    { field: "species", headerName: "Species" },
+    { field: "commonName", headerName: "Common Name" },
+    { field: "lat", headerName: "Latitude" },
+    { field: "long", headerName: "Longitude" },
+  ];
+
   return (
     <Box
       justifyContent="center"
       display="flex"
-      minHeight="50vh"
+      height={300}
+      width={500}
       alignItems="center"
     >
-      <p>LIST</p>
+      <DataGrid rows={rows} columns={columns} />
     </Box>
   );
 }
