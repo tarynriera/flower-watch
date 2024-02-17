04ddb6e955bce32c4f3a5c3e6c843ae69c40146f
Taryn Riera <tarynriera@gmail.com>
2024-02-17 16:49:05 -0500

    testing deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-
 const basePath = process.env.NODE_ENV === "production" ? "/flower-watch" : "";
 
 const nextConfig = {
   basePath,
-  assetPrefix: `${basePath}`,
+  assetPrefix: `${basePath}/`,
   output: "export",
   distDir: "out",
   reactStrictMode: true,
